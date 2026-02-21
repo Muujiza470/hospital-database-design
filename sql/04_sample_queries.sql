4fef925f271de7024b19942d5b88bcee063cf056
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-20 23:49:13 -0500

    Add select and delete queries for ItemCharge
    Added a query to select and delete specific ItemCharge records.

--- sql/04_sample_queries.sql
+++ sql/04_sample_queries.sql
@@ -392,3 +392,13 @@ SELECT TOP (1000) [DoctorID]
       ,[AltPhone]
       ,[Email]
   FROM [HospitaIDictionary].[dbo].[Doctor]
+
+/* ===== From SQLQuery32.sql ===== */
+SELECT TOP (1000) [ChargeID]
+      ,[Date]
+      ,[Quantity]
+      ,[PatientID]
+      ,[ItemID]
+  FROM [HospitaIDictionary].[dbo].[ItemCharge]
+  DELETE FROM ItemCharge
+  WHERE ChargeID IN (1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025)
