e20b60ef8c85db3d5a9185217af55f5ea73d9917
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-20 23:59:52 -0500

    Delete sql/SQLQuery32.sql

--- sql/SQLQuery32.sql
+++
@@ -1,8 +0,0 @@
-SELECT TOP (1000) [ChargeID]
-      ,[Date]
-      ,[Quantity]
-      ,[PatientID]
-      ,[ItemID]
-  FROM [HospitaIDictionary].[dbo].[ItemCharge]
-  DELETE FROM ItemCharge
-  WHERE ChargeID IN (1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025)
