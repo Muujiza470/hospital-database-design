ab68c3d1e2dbdf01d4997e17b74d61e59bf9071c
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-21 00:00:41 -0500

    Delete sql/SQLQuery38.sql

--- sql/SQLQuery38.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [WardID]
-      ,[Location]
-      ,[PhoneNumber]
-      ,[NurseCharged]
-  FROM [HospitaIDictionary].[dbo].[Ward]
-  DELETE FROM Ward
-  WHERE WardID IN (33, 34, 35)
