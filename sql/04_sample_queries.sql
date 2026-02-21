350b26d7ec4957fb8411c62015082c4fa33aee09
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-20 23:35:16 -0500

    Add SQL query to insert patient and select details
    Added a new SQL query to insert a patient record and retrieve patient details.

--- sql/04_sample_queries.sql
+++ sql/04_sample_queries.sql
@@ -186,4 +186,24 @@ SELECT TOP (1000) [BedID]
       ,[Availability]
       ,[WardID]
   FROM [HospitaIDictionary].[dbo].[Bed]
-
+      
+/* ===== From SQLQuery12.sql ===== */
+SELECT TOP (1000) [PatientID]
+      ,[FirstName]
+      ,[LastName]
+      ,[Gender]
+      ,[DOB]
+      ,[Street]
+      ,[City]
+      ,[State]
+      ,[Zip]
+      ,[PhoneNumber]
+      ,[AltPhone]
+      ,[Email]
+      ,[Age]
+      ,[BedID]
+  FROM [HospitaIDictionary].[dbo].[Patient]
+  insert into Patient
+  (FirstName, LastName, Gender, DOB, Street, City, State, Zip, PhoneNumber, Email, BedID)
+  Values 
+  ('Pietro', 'Izzo', 'M', '2002-03-03', 'Mayp', 'Milan', 'WA', '34555', '900456789', 'pipi@izzo.as',1)
