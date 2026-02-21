f12e9c3cc5f4fcb0579bffccd5e938edb56a19ae
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-20 23:33:29 -0500

    Add SQL query to retrieve patient admission details

--- sql/04_sample_queries.sql
+++ sql/04_sample_queries.sql
@@ -167,7 +167,17 @@ SET AssignDate = '2024-12-06',
     WardID = 2,
     NurseID = 3
 WHERE NurseAssignmentID = 1
-
+/* ===== From SQLQuery10.sql ===== */
+SELECT ad.PatientID, ad.AdmitDoc, ad.AdmitDate, p.BedID, b.WardID
+FROM AdmitDischarge ad
+JOIN Patient p
+ON ad.PatientID = p.PatientID
+JOIN Doctor d
+on ad.AdmitDoc = d.DoctorID
+JOIN Bed b
+on p.BedID = b.BedID
+ORDER BY 
+    ad.AdmitDate ASC
 
 
 
