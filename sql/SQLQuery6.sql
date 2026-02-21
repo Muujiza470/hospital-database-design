c6711f42f8ac48ce7ad5504e1d76e40d39728250
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-21 00:01:04 -0500

    Delete sql/SQLQuery6.sql

--- sql/SQLQuery6.sql
+++
@@ -1,6 +0,0 @@
-SELECT TOP (1000) [CertID]
-      ,[Certification]
-  FROM [HospitaIDictionary].[dbo].[Certification]
-Update Certification
-set Certification = 'Emergency Med Technician'
-Where CertID = 2
