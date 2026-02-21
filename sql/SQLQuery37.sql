9046b5736b2d00c052d28faac4f78252b70d7aa9
PreciosaMuujiza <89173788+Muujiza470@users.noreply.github.com>
2026-02-21 00:00:18 -0500

    Delete sql/SQLQuery37.sql

--- sql/SQLQuery37.sql
+++
@@ -1,5 +0,0 @@
-SELECT TOP (1000) [SpecialtyID]
-      ,[Specialty]
-  FROM [HospitaIDictionary].[dbo].[Specialty]
-  DELETE FROM Specialty
-  WHERE SpecialtyID IN (1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020, 1021)
